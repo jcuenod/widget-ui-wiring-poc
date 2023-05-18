603d2f7e144c322a5277e4d6073353c1e696f896
James Cuénod <4253884+jcuenod@users.noreply.github.com>
2023-05-18 09:44:13 -0500

    Add widget demonstrating options api

--- src/components/doric-widgets/Widgets.ts
+++ src/components/doric-widgets/Widgets.ts
@@ -1,6 +1,7 @@
 import TextDisplayWidget from '@/components/doric-widgets/TextDisplayWidget.vue'
 import PassageRefWidget from '@/components/doric-widgets/PassageRefWidget.vue'
 import DictionaryWidget from '@/components/doric-widgets/DictionaryWidget.vue'
+import WordQueryWidget from '@/components/doric-widgets/WordQueryWidget.vue'
 
 export default {
   "text-display-widget": {
@@ -14,5 +15,9 @@ export default {
   "dictionary-widget": {
     defaultLabel: "Dictionary",
     widget: DictionaryWidget as Function
+  },
+  "word-query-widget": {
+    defaultLabel: "Word Query",
+    widget: WordQueryWidget as Function
   },
 } as WidgetComponentMap
---
+++ src/components/doric-widgets/WordQueryWidget.vue
@@ -0,0 +1,27 @@
+<template>
+    <input v-model="osisRef.value" type="text" placeholder="osisRef" />
+    <button @click="submitOsisRef">submit ref</button>
+    <input v-model="selectedLemma.value" type="text" placeholder="lemma" />
+    <button @click="submitSelectedLemma">submit lemma</button>
+</template>
+  
+<script>
+export default {
+    data() {
+        return {
+            osisRef: this.useDoricInput("osisRef"),
+            selectedLemma: this.useDoricInput("selectedLemma")
+        }
+    },
+    props: ['useDoricOutput', 'useDoricInput'],
+    methods: {
+        submitOsisRef() {
+            this.useDoricOutput("osisRef", this.osisRef.value);
+        },
+        submitSelectedLemma() {
+            this.useDoricOutput("selectedLemma", this.selectedLemma.value);
+        }
+    }
+}
+</script>
+  
