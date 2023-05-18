afcdfd1325a000c20a79d873ac585ddd6ddf509b
James Cuénod <4253884+jcuenod@users.noreply.github.com>
2023-05-18 10:07:16 -0500

    Add widget demonstrating options api

---
+++ src/components/doric-widgets/WordQueryWidget.vue
@@ -0,0 +1,24 @@
+<template>
+    <input v-model="osisRef.value" type="text" placeholder="osisRef" />
+    <button @click="submitOsisRef(osisRef.value)">submit ref</button>
+    <input v-model="selectedLemma.value" type="text" placeholder="lemma" />
+    <button @click="submitSelectedLemma(selectedLemma.value)">submit lemma</button>
+</template>
+  
+<script>
+export default {
+    data() {
+        return {
+            osisRef: this.useDoricInput("osisRef"),
+            selectedLemma: this.useDoricInput("selectedLemma")
+        }
+    },
+    created() {
+        // Define input methods
+        this.submitOsisRef = this.useDoricOutput("osisRef")
+        this.submitSelectedLemma = this.useDoricOutput("selectedLemma")
+    },
+    props: ['useDoricOutput', 'useDoricInput'],
+}
+</script>
+  
