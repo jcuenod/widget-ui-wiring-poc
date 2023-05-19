cb93226e903098703611ac94b6e9cb7c5cb2c78c
James Cuénod <4253884+jcuenod@users.noreply.github.com>
2023-05-19 09:03:32 -0500

    Resizable (draggable) columns

--- src/views/DoricFramework.vue
+++ src/views/DoricFramework.vue
@@ -1,5 +1,7 @@
 <script setup>
 import { nextTick, ref, watch } from 'vue'
+import { Splitpanes, Pane } from 'splitpanes'
+import 'splitpanes/dist/splitpanes.css'
 import {
   getUseDoricInput,
   getUseDoricOutput,
@@ -57,7 +59,7 @@ watch(() => props.initialState, (newInitialState) => {
   if (!newInitialState) {
     return
   }
-  const waitForWorkspace = () => { 
+  const waitForWorkspace = () => {
     if (loadingWorkspace.value) {
       nextTick(waitForWorkspace)
       return
@@ -101,47 +103,49 @@ const addWidget = (widgetType, column) => {
 
 <template>
   <div class="doric-widget-framework">
-    <div class="doric-widget-framework__column" v-for="(column, index) in getWorkspaceShape()" :key="index">
-      <div class="doric-widget-framework__widget" :class="{ 'config-mode': configWidget === widget.id }"
-        v-for="(widget) in column" :key="widget.id">
-        <header>
-          <span>
-            {{ !configWidget ? widget.label : widget.id }}
-          </span>
-          <span class="config-button" v-show="!configWidget || configWidget === widget.id">
-            <button v-if="widget?.type in widgets && 'widget' in widgets[widget.type]"
-              @click="() => configureWidget(widget.id)">configure</button>
-            <button @click="() => removeWidget(widget.id)">X</button>
-          </span>
-        </header>
-        <div v-if="configWidget === widget.id">
-          <DoricWidgetConfig :widgetId="widget.id" />
+    <splitpanes>
+      <pane min-size="20" v-for="(column, index) in getWorkspaceShape()" :key="index"
+        :size="100 / getWorkspaceShape().length">
+        <div v-for="(widget) in column" :key="widget.id" class="doric-widget-framework__widget"
+          :class="{ 'config-mode': configWidget === widget.id }">
+          <header>
+            <span>
+              {{ !configWidget ? widget.label : widget.id }}
+            </span>
+            <span class="config-button" v-show="!configWidget || configWidget === widget.id">
+              <button v-if="widget?.type in widgets && 'widget' in widgets[widget.type]"
+                @click="() => configureWidget(widget.id)">configure</button>
+              <button @click="() => removeWidget(widget.id)">X</button>
+            </span>
+          </header>
+          <div v-if="configWidget === widget.id">
+            <DoricWidgetConfig :widgetId="widget.id" />
+          </div>
+          <div :class="{ 'hidden': configWidget === widget.id }">
+            <component v-if="widget?.type in widgets && 'widget' in widgets[widget.type]"
+              :is="widgets[widget.type].widget" :useDoricOutput="param => getUseDoricOutput(widget.id, param)"
+              :useDoricInput="(param, options) => getUseDoricInput(widget.id, param, options)" />
+            <DoricMissingWidget :type="widget?.type" v-else />
+          </div>
         </div>
-        <div :class="{ 'hidden': configWidget === widget.id }">
-          <component v-if="widget?.type in widgets && 'widget' in widgets[widget.type]"
-            :is="widgets[widget.type].widget"
-            :useDoricOutput="param => getUseDoricOutput(widget.id, param)"
-            :useDoricInput="(param, options) => getUseDoricInput(widget.id, param, options)" />
-          <DoricMissingWidget :type="widget?.type" v-else />
+        <div class="add-widget">
+          <div>
+            <button v-if="showWidgetsToAddColumn === -1" @click="() => setColumnToAddWidget(index)">
+              +
+            </button>
+            <button v-else-if="showWidgetsToAddColumn === index" @click="() => setColumnToAddWidget(-1)">
+              x
+            </button>
+          </div>
+          <div v-show="showWidgetsToAddColumn === index" class="add-widget-list">
+            <button v-for="(widgetType) in Object.keys(widgets)" :key="widgetType"
+              @click="() => addWidget(widgetType, index)">
+              {{ widgets[widgetType].defaultLabel }}
+            </button>
+          </div>
         </div>
-      </div>
-      <div class="add-widget">
-        <div>
-          <button v-if="showWidgetsToAddColumn === -1" @click="() => setColumnToAddWidget(index)">
-            +
-          </button>
-          <button v-else-if="showWidgetsToAddColumn === index" @click="() => setColumnToAddWidget(-1)">
-            x
-          </button>
-        </div>
-        <div v-show="showWidgetsToAddColumn === index" class="add-widget-list">
-          <button v-for="(widgetType) in Object.keys(widgets)" :key="widgetType"
-            @click="() => addWidget(widgetType, index)">
-            {{ widgets[widgetType].defaultLabel }}
-          </button>
-        </div>
-      </div>
-    </div>
+      </pane>
+    </splitpanes>
     <div class="column-insert"><button @click="insertColumn(getWorkspaceShape().length)">+</button></div>
   </div>
 </template>
@@ -152,57 +156,50 @@ const addWidget = (widgetType, column) => {
   flex-direction: row;
   margin: 0 0.1rem;
 
-  >.doric-widget-framework__column {
-    display: flex;
-    flex-direction: column;
-    flex-grow: 1;
-    width: 100%;
+  .doric-widget-framework__widget {
+    border: 1px solid #000;
+    border-radius: 3px;
+    margin: 0.2rem;
 
-    >.doric-widget-framework__widget {
-      border: 1px solid #000;
-      border-radius: 3px;
-      margin: 0.2rem;
-
-      &.config-mode {
-        border-color: orange;
-      }
-
-      header {
-        display: flex;
-        flex-direction: row;
-        justify-content: space-between;
-        align-items: center;
-        background-color: #eee;
-        padding: 0.5rem;
-      }
-
-      >div {
-        padding: 0.5rem;
-      }
-
-      .hidden {
-        display: none;
-      }
+    &.config-mode {
+      border-color: orange;
     }
 
-    .add-widget {
+    header {
+      display: flex;
+      flex-direction: row;
+      justify-content: space-between;
+      align-items: center;
+      background-color: #eee;
+      padding: 0.5rem;
+    }
+
+    >div {
+      padding: 0.5rem;
+    }
+
+    .hidden {
+      display: none;
+    }
+  }
+
+  .add-widget {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+
+    .add-widget-list {
       display: flex;
       flex-direction: column;
       align-items: center;
+      background-color: #eee;
+      padding: 0.5rem;
+      margin: 0.2rem;
+      border: 1px solid #000;
+      border-radius: 3px;
 
-      .add-widget-list {
-        display: flex;
-        flex-direction: column;
-        align-items: center;
-        background-color: #eee;
-        padding: 0.5rem;
+      button {
         margin: 0.2rem;
-        border: 1px solid #000;
-        border-radius: 3px;
-
-        button {
-          margin: 0.2rem;
-        }
       }
     }
   }
@@ -211,6 +208,21 @@ const addWidget = (widgetType, column) => {
 .config-button {
   button {
     cursor: pointer;
+  }
+}
+</style>
+
+<style lang="scss" global>
+.splitpanes--vertical>.splitpanes__splitter {
+  box-sizing: border-box;
+  width: 12px;
+  margin: 0 1px;
+  border-left: 4px solid #fff;
+  border-right: 4px solid #fff;
+  background-color: #f8f8f8;
+
+  &:hover {
+    background-color: #ccc;
   }
 }
 </style>
